35dabfd1e948ed5835814854ea714c5a8ad514f1
ERmilburn02 <ERmilburn02@outlook.com>
2020-05-06 15:08:58 +0100

    beginning work on login

---
+++ index.js
@@ -0,0 +1,33 @@
+const https = require('https');
+exports.login = function (username, password) {
+  const data = JSON.stringify({
+    username: username,
+    password: password,
+  });
+
+  const options = {
+    hostname: 'www.floatplane.com',
+    port: 443,
+    path: '/api/auth/login',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+    },
+  };
+
+  const req = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on('error', (error) => {
+    console.error(error);
+  });
+
+  req.write(data);
+  req.end();
+};
